df61df9693f6c2e84ae4e31d002cb66c2c1331da
Jlupuka <olexiyfoma@yandex.ru>
2024-01-07 23:12:46 +0500

    Fully operational user base

--- game_field/game.py
+++ game_field/game.py
@@ -16,6 +16,9 @@ cell_size = 32
 degrees = (0, 180, 90, 270)
 rotate_mapping = {key: degree for degree, key in zip(degrees, config.control.keys())}
 
+count_destroyed_enemy = 0
+count_destroyed_user = 0
+
 
 class BaseSprite(pygame.sprite.Sprite):
     def __init__(self, image: pygame.Surface | list[pygame.Surface], x_pos: int, y_pos: int, *groups) -> None:
@@ -61,7 +64,7 @@ class UserTank(BaseSprite):
     def check_collision(self, x: int, y: int) -> bool:
         new_rect = self.rect.copy()
         new_rect.topleft = (x, y)
-        for sprite in box_sprite.sprites() + stone_sprite.sprites():
+        for sprite in box_sprite.sprites() + stone_sprite.sprites() + enemy_sprite.sprites():
             if new_rect.colliderect(sprite.rect):
                 return True  # Столкновение с объектом
         return False  # Нет столкновения
@@ -73,9 +76,42 @@ class UserTank(BaseSprite):
 
 class EnemyTank(BaseSprite):
     def __init__(self, image: list[pygame.Surface], x_pos: int, y_pos: int) -> None:
-        super().__init__(image, x_pos, y_pos, all_sprite, user_sprite)
+        super().__init__(image, x_pos, y_pos, all_sprite, enemy_sprite)
         self.index_image = 0
+        self.angle = 0
         self.image = self.orig_image[self.index_image]
+        self.rank_move = 1
+
+    def move(self, type_move: str) -> None:
+        new_x, new_y = self.rect.x, self.rect.y  # Сначала предполагаем, что новые координаты будут совпадать с текущими
+        match type_move:
+            case 'up':
+                new_y -= self.rank_move
+            case 'down':
+                new_y += self.rank_move
+            case 'left':
+                new_x -= self.rank_move
+            case 'right':
+                new_x += self.rank_move
+        # Проверяем, не сталкивается ли танк с какими-либо объектами
+        if not self.check_collision(new_x, new_y) and 0 + left <= new_x <= (
+                width - cell_size - left) and 0 + top <= new_y <= (
+                height - cell_size - top // 2):
+            self.rect.topleft = (new_x, new_y)
+            self.index_image = (self.index_image + 1) % len(self.orig_image)
+            self.flip(self.angle)
+
+    def check_collision(self, x: int, y: int) -> bool:
+        new_rect = self.rect.copy()
+        new_rect.topleft = (x, y)
+        for sprite in box_sprite.sprites() + stone_sprite.sprites() + user_sprite.sprites():
+            if new_rect.colliderect(sprite.rect):
+                return True  # Столкновение с объектом
+        return False  # Нет столкновения
+
+    def flip(self, angle: int = None) -> None:
+        self.image = pygame.transform.rotate(self.orig_image[self.index_image], angle)
+        self.angle = angle
 
 
 class Box(BaseSprite):
@@ -83,9 +119,81 @@ class Box(BaseSprite):
         super().__init__(image, x_pos, y_pos, all_sprite, box_sprite)
 
 
-class Patron(BaseSprite):
-    def __init__(self, image: pygame.Surface, x_pos: int, y_pos: int) -> None:
-        super().__init__(image, x_pos, y_pos, all_sprite, patron_sprite)
+class Patron(pygame.sprite.Sprite):
+    def __init__(self, image: pygame.Surface, x_pos: int, y_pos: int, angle: int, user: bool = True) -> None:
+        super().__init__(all_sprite, patron_sprite)
+        self.user = user
+        self.image = image
+        self.rect = self.image.get_rect().move(
+            x_pos, y_pos
+        )
+        self.mask = pygame.mask.from_surface(self.image)
+        self.angle = angle
+        self.wx = False
+        self.wy = False
+        self.angle_move()
+
+    def angle_move(self) -> None:
+        if self.angle == 0:
+            self.rect.x += 12
+            self.rect.y -= 10
+            self.wy = -4
+        elif self.angle == 90:
+            self.rect.x -= 10
+            self.rect.y += 12
+            self.wx = -4
+        elif self.angle == 180:
+            self.rect.x += 12
+            self.rect.y += 34
+            self.wy = 4
+        elif self.angle == 270:
+            self.rect.x += 34
+            self.rect.y += 12
+            self.wx = 4
+        self.image = pygame.transform.rotate(self.image, self.angle)
+
+    def move(self) -> None:
+        new_x, new_y = self.rect.x, self.rect.y
+
+        if self.wx:
+            new_x += self.wx
+        elif self.wy:
+            new_y += self.wy
+
+        # Проверяем, не сталкивается ли танк с какими-либо объектами
+        if not self.check_collision(new_x, new_y) and 0 + left <= new_x <= (
+                width - cell_size + left - 6) and 0 + top <= new_y <= (
+                height - cell_size + top - 6):
+            self.rect.topleft = (new_x, new_y)
+        if (self.rect.bottomleft[1] >= height - cell_size + top - 1 or
+                self.rect.bottomleft[0] <= 0 + left + 4 or
+                self.rect.bottomleft[0] >= width - cell_size + 6 or
+                self.rect.bottomleft[1] <= 0 + top + 12):
+            self.kill()
+
+    def check_collision(self, x: int, y: int) -> bool:
+        global count_destroyed_enemy, count_destroyed_user
+        new_rect = self.rect.copy()
+        new_rect.topleft = (x, y)
+        for sprite in stone_sprite.sprites():
+            if new_rect.colliderect(sprite.rect):
+                self.kill()
+                return True  # Столкновение с камнем
+        for sprite in box_sprite.sprites():
+            if new_rect.colliderect(sprite.rect):
+                self.kill()
+                sprite.kill()
+                return True  # Столкновение с коробкой
+        for sprite in enemy_sprite.sprites():
+            if new_rect.colliderect(sprite.rect):
+                if self.user:
+                    count_destroyed_enemy += 1
+                else:
+                    count_destroyed_user += 1
+                self.kill()
+                sprite.kill()
+                return True  # Столкновение с вражеским танком
+        return False  # Нет столкновения
 
 
 class Stone(BaseSprite):
@@ -106,9 +214,9 @@ def sort_level(board_level: list[list[int]], images: Images) -> UserTank:
     }
     for row, row_data in enumerate(board_level):
         for col, col_data in enumerate(row_data):
+            Dirt(image=images.default, x_pos=col, y_pos=row)
             match col_data:
                 case pygame.K_0:
-                    Dirt(image=images.default, x_pos=col, y_pos=row)
                     tanks['user'].append((col, row))
                 case pygame.K_1:
                     Stone(image=images.stone, x_pos=col, y_pos=row)
@@ -117,8 +225,6 @@ def sort_level(board_level: list[list[int]], images: Images) -> UserTank:
                 case pygame.K_3:
                     Dirt(image=images.default, x_pos=col, y_pos=row)
                     tanks['enemy'].append((col, row))
-                case _:
-                    Dirt(image=images.default, x_pos=col, y_pos=row)
     for key in tanks.keys():
         for value in tanks[key]:
             if key == 'user':
@@ -143,6 +249,11 @@ def main(filename: str) -> None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    Patron(images.patron, player.rect.x, player.rect.y, angle)
+        for bullet in patron_sprite.sprites():
+            bullet.move()
         keys = pygame.key.get_pressed()
         if keys[config.control['up']]:
             target_angle = rotate_mapping['up']
